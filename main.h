bf431084ebc28d986e0f713a717103a0e23832d1
Selvy <selvyatef2004@gmail.com>
2023-10-18 15:08:47 -0700

    1-simple_shell.c

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUF_FLUSH '\0'
+
+/* Function prototypes */
+int _sabo1len(char *sabo);
+int _strcmp(char *sabo1, char *sabo2);
+char *starts_with(const char *usopp, const char *kaia);
+char *_strcat(char *hancoc, char *chopper);
+
+#endif /* SHELL_H */
+
